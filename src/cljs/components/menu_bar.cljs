26b5c9e8e5391d29179f4f23eb2ede407c772bea
marcvincenti <vincentimarc@gmail.com>
2017-04-19 00:27:21 +0200

    add dropdown menu in navbar for future coin selection

--- src/cljs/components/menu_bar.cljs
+++ src/cljs/components/menu_bar.cljs
@@ -6,16 +6,17 @@
     [:nav {:class "navbar navbar-default navbar-fixed-top"}
       [:div {:class "container"}
         [:div {:class "navbar-header"}
-          [:button {:type "button" :class "navbar-toggle collapsed"
-                    :data-toggle "collapse" :aria-expanded "false"
-                    :data-target "#bs-example-navbar-collapse-1"}
-            [:span {:class "icon-bar"}]
-            [:span {:class "icon-bar"}]
-            [:span {:class "icon-bar"}]]
-          [:a {:class "navbar-brand" :href "#/"}
-            [:img {:id "logo-pivx" :src "assets/PIVX.png" :alt "PIVX logo"}]]]
+          [:ul {:class "nav navbar-nav"}
+            [:li {:class "dropdown"}
+              [:button {:class "navbar-brand" :href "#" :data-toggle "dropdown"}
+                [:img {:id "logo-pivx" :src "assets/PIVX.png" :alt "PIVX logo"}]
+                [:span {:class "caret"}]]
+              [:ul {:class "dropdown-menu" :role "menu"}
+                [:li [:a {:href "#"} "Dash (work in progress)"]]
+                [:li [:a {:href "#"} "Crown (work in progress)"]]
+                [:li [:a {:href "#"} "Mue (work in progress)"]]]]]]
 
-  [:div {:class "collapse navbar-collapse" :id "bs-example-navbar-collapse-1"}
+  [:div {:class "collapse navbar-collapse"}
 
     [:ul {:class "nav navbar-nav"}
       [:li (active? :calculator) [:a {:href "#/"} "Calculator"]]
