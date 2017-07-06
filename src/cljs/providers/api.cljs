d448da63f7ffaec3fc57b4c82ae6b15e87b9081a
marcvincenti <vincentimarc@gmail.com>
2017-07-06 19:24:44 +0200

    add gbp currency

--- src/cljs/providers/api.cljs
+++ src/cljs/providers/api.cljs
@@ -4,7 +4,7 @@
             [cljs.core.async :refer [<!]]
             [app.state :refer [app-state]]))
 
-(def cur-available ["BTC", "EUR", "USD"])
+(def cur-available ["BTC", "EUR", "USD", "GBP"])
 (def ^:private url "https://318h5of2kh.execute-api.eu-west-1.amazonaws.com/dev/currency/pivx")
 
 (defn update-data
@@ -16,10 +16,12 @@
             supply (get-in response [:body :available_supply])
             btc (get-in response [:body :price_btc])
             eur (get-in response [:body :price_eur])
-            usd (get-in response [:body :price_usd])]
+            usd (get-in response [:body :price_usd])
+            gbp (get-in response [:body :price_gbp])]
         (do
           (swap! app-state assoc-in [:calc :masternodes] mn)
           (swap! app-state assoc-in [:calc :supply] supply)
           (swap! app-state assoc-in [:currencies :btc] btc)
           (swap! app-state assoc-in [:currencies :eur] eur)
+          (swap! app-state assoc-in [:currencies :gbp] gbp)
           (swap! app-state assoc-in [:currencies :usd] usd)))))))
